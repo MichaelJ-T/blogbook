b4285238b2f6316eb7424f8d311dc4c1b6c69148
MichaelJ-T <59335490+MichaelJ-T@users.noreply.github.com>
2021-12-07 14:54:52 +0100

    CI unit tests (fake tests)

---
+++ tests/unit/LoginTest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+ class SampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testIfTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+    public function testIfTrue2(): void
+    {
+        $this->assertTrue(true);
+    }
+    public function testIfTrue3(): void
+    {
+        $this->assertTrue(true);
+    }
+    public function testIfTrue4(): void
+    {
+        $this->assertTrue(true);
+    }
+    public function testIfTrue5(): void
+    {
+        $this->assertTrue(true);
+    }
+}
